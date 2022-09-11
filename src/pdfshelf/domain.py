3851d2a9e40dc693a8ea3839eb51c67d268a35bc
Arthurpmrs <arthurpmrs@gmail.com>
2022-09-11 11:46:44 -0300

    Cleaned up domain.py.

--- src/pdfshelf/domain.py
+++ src/pdfshelf/domain.py
@@ -60,28 +60,3 @@ class Paper(Document):
     volume:int = None
     issn: str = None
     edition: int = None
-
-
-if __name__ == "__main__":
-    folder = {
-        "name": "foldinha",
-        "path": "/home/Documents/Library/Livros"
-    }
-
-    book = Book(
-        title="Modeling and simulation in Python",
-        authors=["Jason M. Kinser"],
-        year=2022,
-        lang="en",
-        filename="Jason M. Kinser - Modeling and Simulation in Python-Chapman & Hall (2022).pdf",
-        ext="pdf",
-        storage_path="teste/Jason M. Kinser - Modeling and Simulation in Python-Chapman & Hall (2022).pdf",
-        folder=folder,
-        size=100000,
-        publisher="CRC Press",
-        isbn13="978-1-032-11648-8"
-    )
-
-
-    print(book)
-    print(book.folder)
