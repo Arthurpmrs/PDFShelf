35eb4acaa7627c8ea4dca2bdfeb442d4c5e337e6
Arthurpmrs <arthurpmrs@gmail.com>
2022-09-24 21:22:14 -0300

    Increase logging file maxBytes to 2.5 MB

--- src/pdfshelf/__init__.py
+++ src/pdfshelf/__init__.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.DEBUG)
 f_format = logging.Formatter('%(asctime)s %(name)-22s %(levelname)-8s [%(lineno)-3s] %(message)s', "%Y-%m-%d %H:%M")
 s_format = logging.Formatter('%(name)-22s %(levelname)-8s [%(lineno)-3s] %(message)s')
 
-f_handler = RotatingFileHandler(config_folder / "pdfshelf.log", maxBytes=5000, backupCount=25)
+f_handler = RotatingFileHandler(config_folder / "pdfshelf.log", maxBytes=2500000, backupCount=25)
 f_handler.setLevel(logging.DEBUG)
 f_handler.setFormatter(f_format)
 
