56cee9d28fa133129d3a14a2e45715604b1402f1
Arthurpmrs <arthurpmrs@gmail.com>
2022-10-10 10:27:57 -0300

    Fix data folder not being created

--- src/pdfshelf/config.py
+++ src/pdfshelf/config.py
@@ -5,3 +5,4 @@ config_folder = user_folder / ".config" / "pdfshelf"
 config_folder.mkdir(exist_ok=True)
 # Create a config file with config parser to make possible to alter this values. For now:
 default_document_folder = user_folder / "Documents" / "PDFShelf"
+default_document_folder.mkdir(exist_ok=True)
