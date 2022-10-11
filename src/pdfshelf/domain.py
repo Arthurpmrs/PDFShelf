3370af00b08a1ce1c25af7a0f86fc8adceb13135
Arthurpmrs <arthurpmrs@gmail.com>
2022-10-11 20:59:46 -0300

    Change Book and Folder classes
    In this change, Book and Folder classes are now
    responsible for converting database incoming data
    to conform to its type definitions. Changes were
    made to get_parsed_dict on Book cls, so now list
    objects are parsed to string via json.dumps method.

--- src/pdfshelf/domain.py
+++ src/pdfshelf/domain.py
@@ -1,3 +1,4 @@
+import json
 import hashlib
 from pathlib import Path
 from datetime import datetime
@@ -8,6 +9,7 @@ class Folder:
     name:str
     path: Path
     added_date: datetime = datetime.now()
+    folder_id: int = None
     active: bool = True
 
     def __post_init__(self):
@@ -16,6 +18,9 @@ class Folder:
 
         if isinstance(self.added_date, str):
             self.added_date = datetime.fromisoformat(self.added_date)
+
+        if isinstance(self.active, int):
+            self.active = True if self.active == 1 else False
         
     def get_parsed_dict(self) -> dict[str, Any]:
         d = { **self.__dict__ }
@@ -25,7 +30,7 @@ class Folder:
         return d
 
 @dataclass(kw_only=True)
-class Document:
+class Book:
     title: str
     authors: list[str]
     year: int
@@ -36,22 +41,35 @@ class Document:
     folder: Folder
     size: float
     tags: list[str]
+    publisher: str
     added_date: datetime = datetime.now()
-    hash_id: str = ""
+    book_id: int = None
     active: bool = True
     confirmed: bool = False
+    isbn13: str = None
+    parsed_isbn: str = None
+    hash_id: str = None
 
     def __post_init__(self):
+        if isinstance(self.authors, str):
+            self.authors = json.loads(self.authors)
+        
         if isinstance(self.storage_path, str):
             self.storage_path = Path(self.storage_path)
 
-        if isinstance(self.added_date, str):
-            self.added_date = datetime.fromisoformat(self.added_date)
-
         if isinstance(self.folder, dict):
             self.folder = Folder(**self.folder)
+        
+        if isinstance(self.tags, str):
+            self.tags = json.loads(self.tags)
 
-        if self.hash_id == "":
+        if isinstance(self.added_date, str):
+            self.added_date = datetime.fromisoformat(self.added_date)
+        
+        if isinstance(self.active, int):
+            self.active = True if self.active == 1 else False
+        
+        if self.hash_id is None:
             self.hash_id = hashlib.md5(self.filename.encode()).hexdigest()
 
     def get_absolute_path(self) -> Path:
@@ -59,28 +77,11 @@ class Document:
 
     def get_parsed_dict(self) -> tuple[dict[str, Any], dict[str, Any]]:
         d = { **self.__dict__ }
-        d["authors"] = str(d["authors"])
+        d["authors"] = json.dumps(d["authors"])
         d["storage_path"] = str(d["storage_path"])
-        d["tags"] = str(d["tags"])
+        d["tags"] = json.dumps(d["tags"])
         d["added_date"] = str(d["added_date"])
         d["active"] = 1 if d["active"] else 0
         d["confirmed"] = 1 if d["confirmed"] else 0
         folder = d.pop("folder")
         return d, folder.get_parsed_dict()
-
-@dataclass
-class Book(Document):
-    publisher: str
-    isbn10: str = None
-    isbn13: str = None
-    parsed_isbn: str = None
-
-
-@dataclass
-class Paper(Document):
-    journal:str
-    doi: str
-    pages:tuple[int,int] = None
-    volume:int = None
-    issn: str = None
-    edition: int = None
