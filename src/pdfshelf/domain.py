b2b0a05fe2af6e038ce13cbbb4eb546c8db9ec88
Arthurpmrs <arthurpmrs@gmail.com>
2023-02-21 11:44:16 -0300

    Improve Book and Folder dataclasses
    Book and Folder classes were improved using the from_raw_data
    strategy. This change was implemented together with the addition
    of the Book.cover_path field, that would result in breaking
    changes anyway. The change in strategy results in better typing.

--- src/pdfshelf/domain.py
+++ src/pdfshelf/domain.py
@@ -1,28 +1,31 @@
 import json
 import hashlib
+from typing import Any
 from pathlib import Path
 from datetime import datetime
 from dataclasses import dataclass
-from typing import Any
 
 
-@dataclass
+@dataclass(kw_only=True)
 class Folder:
-    name: str
-    path: Path | str
-    added_date: datetime | str = datetime.now()
     folder_id: int | None = None
-    active: bool | int = True
+    name: str
+    path: Path
+    added_date: datetime = datetime.now()
+    active: bool = True
 
-    def __post_init__(self):
-        if isinstance(self.path, str):
-            self.path = Path(self.path)
+    @classmethod
+    def from_raw_data(cls, data: dict[str, Any]):
+        if isinstance(data.get("path"), str):
+            data["path"] = Path(data["path"])
 
-        if isinstance(self.added_date, str):
-            self.added_date = datetime.fromisoformat(self.added_date)
+        if isinstance(data.get("added_date"), str):
+            data["added_date"] = datetime.fromisoformat(data["added_date"])
 
-        if isinstance(self.active, int):
-            self.active = True if self.active == 1 else False
+        if isinstance(data.get("active"), int):
+            data["active"] = True if data["active"] == 1 else False
+
+        return cls(**data)
 
     def get_parsed_dict(self) -> dict[str, Any]:
         d = {**self.__dict__}
@@ -31,60 +34,66 @@ class Folder:
         return d
 
 
-@dataclass(kw_only=True)
+@ dataclass(kw_only=True)
 class Book:
-    title: str
-    authors: list[str] | str
+    book_id: int | None = None
+    hash_id: str | None = None
+    title: str | None
+    authors: list[str]
     year: int | None
-    lang: str
+    lang: str | None
+    publisher: str | None
+    isbn13: str | None
+    parsed_isbn: str | None
+    folder: Folder
     filename: str
     ext: str
-    storage_path: Path | str
-    folder: Folder | dict
+    storage_path: Path
     size: float
-    tags: list[str] | str
-    publisher: str
-    # cover: Path | str
-    added_date: datetime | str = datetime.now()
-    book_id: int | None = None
-    active: bool | int = True
-    confirmed: bool | int = False
-    isbn13: str | None = None
-    parsed_isbn: str | None = None
-    hash_id: str | None = None
+    tags: list[str]
+    added_date: datetime = datetime.now()
+    cover_path: Path | None
+    active: bool = True
+    confirmed: bool = False
 
     def __post_init__(self):
-        if isinstance(self.authors, str):
-            self.authors = json.loads(self.authors)
-
-        if isinstance(self.storage_path, str):
-            self.storage_path = Path(self.storage_path)
-
-        if isinstance(self.folder, dict):
-            self.folder = Folder(**self.folder)
-
-        if isinstance(self.tags, str):
-            self.tags = json.loads(self.tags)
-
-        if isinstance(self.added_date, str):
-            self.added_date = datetime.fromisoformat(self.added_date)
-
-        if isinstance(self.active, int):
-            self.active = True if self.active == 1 else False
-
-        if isinstance(self.confirmed, int):
-            self.confirmed = True if self.confirmed == 1 else False
-
         if self.hash_id is None:
             self.hash_id = hashlib.md5(self.filename.encode()).hexdigest()
 
-    def get_absolute_path(self) -> Path:
-        return self.folder.path / self.storage_path  # type: ignore
+    @classmethod
+    def from_raw_data(cls, data: dict[str, Any]):
+        if isinstance(data.get("authors"), str):
+            data["authors"] = json.loads(data["authors"])
+
+        if isinstance(data.get("storage_path"), str):
+            data["storage_path"] = Path(data["storage_path"])
+
+        if isinstance(data.get("cover_path"), str):
+            data["cover_path"] = Path(data["cover_path"])
+
+        if isinstance(data.get("tags"), str):
+            data["tags"] = json.loads(data["tags"])
+
+        if isinstance(data.get("added_date"), str):
+            data["added_date"] = datetime.fromisoformat(data["added_date"])
+
+        if isinstance(data.get("active"), int):
+            data["active"] = True if data["active"] == 1 else False
+
+        if isinstance(data.get("confirmed"), int):
+            data["confirmed"] = True if data["confirmed"] == 1 else False
+
+        if isinstance(data.get("folder"), dict):
+            data["folder"] = Folder.from_raw_data(data["folder"])
+
+        return cls(**data)
 
     def get_parsed_dict(self) -> tuple[dict[str, Any], dict[str, Any]]:
         d = {**self.__dict__}
         d["authors"] = json.dumps(d["authors"])
         d["storage_path"] = str(d["storage_path"])
+        if (d["cover_path"]) is not None:
+            d["cover_path"] = str(d["cover_path"])
         d["tags"] = json.dumps(d["tags"])
         d["active"] = 1 if d["active"] else 0
         d["confirmed"] = 1 if d["confirmed"] else 0
