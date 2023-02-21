a1a3f8abe092e6ac0658744e3fa82dde823fa363
Arthurpmrs <arthurpmrs@gmail.com>
2023-02-21 16:51:39 -0300

    Improve config.py which now relies on configparser

--- src/pdfshelf/config.py
+++ src/pdfshelf/config.py
@@ -1,8 +1,34 @@
 import os
+import configparser
 from pathlib import Path
+
 user_folder = Path(os.path.expanduser("~"))
 config_folder = user_folder / ".config" / "pdfshelf"
 config_folder.mkdir(exist_ok=True)
-# Create a config file with config parser to make possible to alter this values. For now:
-default_document_folder = user_folder / "Documents" / "PDFShelf"
+
+
+def create_base_config_file() -> None:
+    config_file_path = config_folder / "config.ini"
+    if config_file_path.exists():
+        return
+
+    default_document_folder = user_folder / "Documents" / "PDFShelf"
+    conf = configparser.ConfigParser()
+    conf["DEFAULT"] = {
+        "default_document_folder": str(default_document_folder),
+        "cover_folder": str(default_document_folder / "cover")
+    }
+
+    with open(config_file_path, 'w') as configfile:
+        conf.write(configfile)
+
+
+create_base_config_file()
+config = configparser.ConfigParser()
+config.sections()
+config.read(config_folder / "config.ini")
+
+default_document_folder = Path(config["DEFAULT"]["default_document_folder"])
 default_document_folder.mkdir(exist_ok=True)
+COVER_FOLDER = Path(config["DEFAULT"]["cover_folder"])
+COVER_FOLDER.mkdir(exist_ok=True)
